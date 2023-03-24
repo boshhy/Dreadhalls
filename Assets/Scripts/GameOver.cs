a6c92ea899a91cf376ef12083d5c60f085b64f50
boshhy <boshhy101@yahoo.com>
2023-03-23 17:56:13 -0700

    Player now has a game over screen when falling through floor

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -4){
+            //gameObject.GetComponent<Transform>();
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
